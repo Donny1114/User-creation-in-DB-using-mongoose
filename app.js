0d7a3d560bb0e14ee5b7edc47c56980ef6c9f0d4
dionn11 <dpenanueva8@hotmail.com>
2022-09-07 18:03:18 +0400

    Revert "Salting & Hashing lvl 4"
    This reverts commit 2dc2c43e3952ac7df71f6a51e884ed58dbb2b552.

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ const bodyParser= require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
-// const md5 = require("md5");
-const bcrypt = require("bcrypt")
-const saltRounds = 10;
+const md5 = require("md5");
 
 // console.log(process.env)
 
 const app = express();
-// console.log(process.env.API_KEY);
+console.log(process.env.API_KEY);
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -27,7 +26,11 @@ const userSchema = new mongoose.Schema( {
   password: String
 });
 
+
+
 // userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,46 +47,43 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req, res,){
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets")
-    }
+  const newUser = new User({
+    email: req.body.username,
+    password: md5(req.body.password)
   });
+newUser.save(function(err){
+  if(err){
+    console.log(err);
+  }else{
+    res.render("secrets")
+  }
 });
-
 });
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password)
   User.findOne({email: username}, function(err, foundUser){
     if (err){
       console.log(err);
     }else {
       if (foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if(result=== true){
-          res.render("secrets");
-          }
-  });
-
+        if (foundUser.password === password){
+          res.render("secrets")
         }else {
           if(foundUser.password!== password){
             res.render("errorlogin")
           }
         }
       }
-
+    }
   });
 });
+
+
+
+
+
 
 
 app.listen(3000, function(){
